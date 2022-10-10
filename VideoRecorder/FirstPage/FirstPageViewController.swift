1de7420537cb7693e0304206eaa7f81a90de8b0b
ecc414 <ecc00414@gmail.com>
2022-10-11 00:30:44 +0900

    feat : 두 번째 화면으로 이동 구현

--- VideoRecorder/FirstPage/FirstPageViewController.swift
+++ VideoRecorder/FirstPage/FirstPageViewController.swift
@@ -12,9 +12,33 @@ class FirstPageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.widthAnchor.constraint(equalToConstant: 200),
+            button.heightAnchor.constraint(equalToConstant: 200)
+        ])
         
-        view.backgroundColor = .green
+    
         // Do any additional setup after loading the view.
+    }
+    
+    lazy var button : UIButton = {
+       let btn = UIButton()
+        btn.setTitle("녹화하기", for: .normal)
+        btn.addTarget(self, action: #selector(moveToSecondPage), for: .touchUpInside)
+        btn.backgroundColor = .cyan
+        btn.layer.cornerRadius = 5
+        return btn
+    }()
+    
+    @objc func moveToSecondPage(){
+        let secondVC = SecondPageViewController()
+        secondVC.modalTransitionStyle = .crossDissolve
+        secondVC.modalPresentationStyle = .fullScreen
+        present(secondVC, animated: true)
     }
     
 }
---
+++ VideoRecorder/SecondPage/SecondPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondPageViewController.swift
+//  VideoRecorder
+//
+//  Created by 엄철찬 on 2022/10/11.
+//
+
+import UIKit
+
+class SecondPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
